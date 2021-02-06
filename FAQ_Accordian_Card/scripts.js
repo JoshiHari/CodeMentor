a80d19cffb6e8956bd2f95e1ffa337b4a270ab69
Storm <joshihari4@gmail.com>
2021-02-06 19:53:10 +0530

    !st commit

---
+++ FAQ_Accordian_Card/scripts.js
@@ -0,0 +1,26 @@
+function answer(curEle){
+ console.log(curEle);
+  
+ ans = curEle.nextElementSibling.style.display;
+
+ console.log(ans);
+
+ if(ans=="none"){
+     curEle.nextElementSibling.style.display="flex";
+ }
+ else{
+     curEle.nextElementSibling.style.display="none";
+ }
+    
+
+
+
+}
+
+function hidepara(){
+    para = document.querySelectorAll(".ans");
+    
+    for(i=0; i< para.length;i++){
+        para[i].style.display = "none";
+    }
+}
